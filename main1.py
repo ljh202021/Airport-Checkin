8f84e777174e39a817ed283f3de5fa1a5e39c5e9
ljh202021 <73688334+ljh202021@users.noreply.github.com>
2023-07-22 01:12:05 +0800

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import os
+
+requests.packages.urllib3.disable_warnings()
+
+def checkin():
+    email='ljh_alter@163.com'
+    password='123456789'
+    base_url='https://portx.cc'
+    email = email.split('@')
+    email = email[0] + '%40' + email[1]
+    session = requests.session()
+    session.get(base_url, verify=False)
+    login_url = base_url + '/auth/login'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/56.0.2924.87 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    }
+    post_data = 'email=' + email + '&passwd=' + password + '&code='
+    post_data = post_data.encode()
+    response = session.post(login_url, post_data, headers=headers, verify=False)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/56.0.2924.87 Safari/537.36',
+        'Referer': base_url + '/user'
+    }
+    response = session.post(base_url + '/user/checkin', headers=headers,
+                            verify=False)
+    response = json.loads(response.text)
+    print(response['msg'])
+    return response['msg']
+
+
+result = checkin()
+if SCKEY != '':
+    sendurl = 'https://sctapi.ftqq.com/' + SCKEY + '.send?title=机场签到&desp=' + result
+    r = requests.get(url=sendurl)
+if TG_USER_ID != '':
+    sendurl = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage?chat_id={TG_USER_ID}&text={result}&disable_web_page_preview=True'
+    r = requests.get(url=sendurl)
